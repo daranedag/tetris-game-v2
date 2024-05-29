1ff2db70a07cf8e20a5dbca0d8cbea922f031293
Diego Araneda <daranedag@gmail.com>
2024-05-29 00:31:41 -0400

    Tetris funcionando, listo para enchular

---
+++ consts.js
@@ -0,0 +1,42 @@
+export const BLOCK_SIZE = 20
+export const BOARD_WIDTH = 14
+export const BOARD_HEIGHT = 30
+
+export const EVENT_MOVEMENTS = {
+    LEFT: 'ArrowLeft',
+    DOWN: 'ArrowDown',
+    RIGHT: 'ArrowRight',
+    UP: 'ArrowUp'
+}
+
+export const PIECES = [
+    [
+      [1, 1],
+      [1, 1]  
+    ],
+    [
+      [1, 1, 1, 1]
+    ],
+    [
+      [0, 1, 0],
+      [1, 1, 1]
+    ],
+    [
+      [1, 1, 0],
+      [0, 1, 1]
+    ],
+    [
+      [1, 1],
+      [0, 1],
+      [0 ,1]
+    ],
+    [
+      [1, 1],
+      [1, 0],
+      [1, 0]
+    ]
+  ]
+
+  export const COLORS = [
+    'red', 'green', 'yellow', 'blue', 'orange'
+  ]
